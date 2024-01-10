744b2fe733ef8cfbd9591696b7d56eb97d2f922b
merajHossain <mh1669101@gmail.com>
2024-01-10 20:42:20 +0600

    navbar section done

--- src/Components/CardDeal.jsx
+++ src/Components/CardDeal.jsx
@@ -3,13 +3,8 @@ import React from 'react';
 const CardDeal = () => {
   return (
     <div>
-      <div class="card">
-        <img class="card-img-top" src="holder.js/100x180/" alt="Title" />
-        <div class="card-body">
-          <h4 class="card-title">Title</h4>
-          <p class="card-text">Text</p>
-        </div>
-      </div>
+      
+     card
       
     </div>
   );
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,10 +1,57 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import logo from "../assets/logo.svg";
+import close from "../assets/close.svg";
+import menu from "../assets/menu.svg";
+import { navLinks } from "../Constants/data";
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
-    <div>
-      navbar
-    </div>
+    <nav className="w-full flex py-6 justify-between items-center navbar">
+      <img className="w-[124px] h-[32px]" src={logo} alt="HooBank logo" />
+
+      <ul className="list-none sm:flex hidden justify-end items-center flex-1">
+        {navLinks.map((nav, index) => (
+          <li
+            key={nav.id}
+            className={`text-white font-poppins font-normal cursor-pointer text-[16px] ${
+              index === navLinks.length - 1 ? "mr-0" : "mr-10"
+            }`}
+          >
+            <a href={`#${nav.id}`}>{nav.title}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => {
+            setToggle(!toggle);
+          }}
+          alt=""
+        />
+
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient  absolute top-20 right-0 mx-4 my-2 min-w[140px] rounded-xl sidebar`}
+        >
+          <ul className="list-none flex flex-col  justify-end items-center flex-1">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`text-white font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                }`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 };
 
