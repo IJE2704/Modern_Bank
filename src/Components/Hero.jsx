083523178011d4b89ee530e7464316bbf03ed753
merajHossain <mh1669101@gmail.com>
2024-01-11 08:56:48 +0600

    hero section done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -17,18 +17,33 @@ const Hero = () => {
             <span className="text-white">1 MONTH</span> ACCOUNT
           </p>
         </div>
-
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="text-white flex-1 font-poppins font-semibold ss:text-[72px] ss:leading-[100px] leading-[75px] text-[52px] ">
             The Next <br className="sm:block hidden" />
             <span className="text-gradient">Generation</span>{" "}
-            <br className="sm:block hidden" /> Payment Method.
-          
           </h1>
-         <div className="ss:flex hidden md:mr-4">
-         <GetStarted></GetStarted>
-         </div>
+          <div className="ss:flex hidden md:mr-4">
+            <GetStarted></GetStarted>
+          </div>
         </div>
+        <h1 className="text-white font-poppins font-semibold ss:text-[68px] ss:leading-[100px] leading-[75px] text-[52px]">
+          Payment Method.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          Our team of experts uses a methodology to identify the credit cards
+          most likely to fit your needs. We examine annual percentage rates,
+          annual fees.
+        </p>
+      </div>
+      <div className={`flex flex-1 ${styles.flexCenter} md:my-0 my-10 relative `}>
+        <img className="w-[100%] h-[100%] relative z-[5]" src="https://i.ibb.co/zR6Hp46/robot.png" alt="" />
+
+        <div className="absolute z-[0] w-[40%] h-[35%] top-20 pink__gradient "></div>
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient "></div>
+        <div className="absolute z-[0] w-[50%] h-[50%]  right-20 bottom-20 blue__gradient "></div>
+      </div>
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted/>
       </div>
     </section>
   );
