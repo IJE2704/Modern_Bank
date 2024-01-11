0a820d3536a2c9729abb005dcea7ca852574c3fc
merajHossain <mh1669101@gmail.com>
2024-01-11 16:40:30 +0600

    testimonial section done

--- src/Components/FeadBackCard.jsx
+++ src/Components/FeadBackCard.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { quotes } from '../assets';
+import styles from '../style';
 
-const FeadBackCard = () => {
+const FeadBackCard = ({content,title,name,img}) => {
   return (
-    <div>
-      feadback
+    <div className='flex justify-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card'>
+      <img className='w-[42px] h-[27px] object-contain' src={quotes} alt="" />
+      <p className='font-poppins font-normal text-[18px] leading-[32px] text-white my-10'>{content}</p>
+      <div className='flex justify-start items-center'>
+        <img className='w-[48px] h-[48px] object-contain' src={img} alt="" />
+        <div className='flex flex-col justify-start ml-4'>
+          <h4 className='font-poppins font-semibold text-[20px] leading-[32px] text-white '> {name}</h4>
+          <p className={`${styles.paragraph}`}>{title}</p>
+        </div>
+      </div>
     </div>
   );
 };
